f75cf90b3eb657d834be08f73a8561c92cbe2928
Jeff Ogata <jeffogata@outlook.com>
2016-01-28 16:53:06 -0600

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace aspnet_logging_01_cli
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<MyClass>();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Debug;
+            
+            loggerFactory.AddConsole(LogLevel.Debug);
+
+            app.Run(async context =>
+            {
+                var myClass = context.RequestServices.GetService<MyClass>();
+
+                myClass.DoSomething(1);
+                myClass.DoSomething(20);
+                myClass.DoSomething(-20);
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
